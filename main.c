e8749f0bcdef3c7b3f5ce158b36b96c522a77faa
Adriano <adriano.carvalho9@usp.br>
2022-05-10 12:36:27 -0300

    10/05 - Esquematizando o projeto e criando a parte visual de printf's

--- main.c
+++ main.c
@@ -1,9 +1,67 @@
 #include <stdio.h>
 
+ // ESPAÇO PARA CRIAR FUNÇÕES
+
 int main(){
 
-//gabriel
-    printf("hello word breno! \n");
+    int id; // VARIAVEL PARA PEGAR ID DO FUNCIONÁRIO
+
+    printf("______________________________ \n");
+    printf("|                            | \n");
+    printf("| Insira o ID do funcionário | \n");
+    printf("|                            | \n");
+    printf("             ");
+    scanf("%d", &id);
+    printf("|                            | \n");
+    printf("|                            | \n");
+    printf("|____________________________|\n");
+
+
+    // IF PARA VER SE O FUNCIONARIO VAI SER DE CADASTROS
+    if(id == 1){
+
+        int a;
+
+        printf("______________________________ \n");
+        printf("|                            | \n");
+        printf("|    1. Cadastrar produto    | \n");
+        printf("|    2. Cadastrar venda      | \n");
+        printf("|                            | \n");
+        printf("             ");
+        scanf("%d", &a);
+        printf("|                            | \n");
+        printf("|                            | \n");
+        printf("|____________________________|\n");
+
+            // IF PARA ENTRAR NO CADASTROD DE PRODUTOS
+            if(a == 1){
+                
+                printf("Cadastrar produtos \n");
+
+            } 
+            
+            // ELSE PARA CADASTRO DE VENDAS
+            else {
+                printf("Cadastrar vendas \n");
+            }
+
+    }
+    
+    // ELSE CASO O FUNCIONARIO SEJA DE VISUALIZAÇÃO DE ESTOQUE
+    else {
+
+        // TABELA DE VISUAL DOS PRODUTOS INSERIDOS NO ESTOQUE
+        printf("_________________________________ \n");
+        printf("|  ID  |  QUANTIDADE  | OUTROS  | \n");
+        printf("|______|______________|_________| \n");
+        printf("|      |              |         | \n");
+        printf("|      |              |         | \n");
+        printf("|      |              |         | \n");
+        printf("|      |              |         | \n");
+        printf("|      |              |         | \n");
+        printf("|______|______________|_________| \n");
+        
+    }
 
 
     return 0;
