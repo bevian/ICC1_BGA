4655a283fa2500626c2a3dab2c07431260ab45bf
brenocamposs <brenovaz.arruda@usp.br>
2022-05-19 14:23:10 -0300

    inserindo bibioteca de acentos

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <locale.h>
  // ESPAÇO PARA CRIAR FUNÇÕES
 
 int main(){
+    setlocale(LC_ALL, "Portuguese_Brazil"); //PERMITE ACENTOS
     
     int id; // VARIAVEL PARA PEGAR ID DO FUNCIONÁRIO
 
